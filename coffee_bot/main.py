c988e84f2836eece43cc99da8f72c04471ddc40a
Nikhil Shetty <84284014+wiznikvibe@users.noreply.github.com>
2022-12-27 16:10:17 +0530

    Add files via upload

---
+++ coffee_bot/main.py
@@ -0,0 +1,19 @@
+"""
+1. Print Report
+2. Check resources sufficient?
+3. Process coins.
+4. Check transaction if successful?
+5. Make Coffee
+"""
+from menu import Menu, MenuItem
+from coffeemaker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+
+
+
+
+
+
